ce63671bb129272b6e84b447cd76699964aa065a
lam <lamnguyenhoang@gmail.com>
2022-12-02 11:29:34 +0100

    Day 2: small refactor

--- aoc.py
+++ aoc.py
@@ -9,7 +9,7 @@ TEST_INPUT_DIRECTORY = "./inputs/test/"
 INPUT_DIRECTORY = "./inputs/"
 
 
-def get_input_path(filename, test, day):
+def get_input_path(day, test, filename):
     path = TEST_INPUT_DIRECTORY if test else INPUT_DIRECTORY
     filename = filename if filename else f"{day}.txt"
     return path + filename
@@ -21,17 +21,17 @@ def get_input_path(filename, test, day):
 @click.option("--filename", default="", help="Input file name")
 def run(day, test, filename):
     if not day:
+        # Use today's day of the month
         day = datetime.now().strftime("%d")
 
-    print(f"Running AOC day {day}{' IN TEST MODE' if test else ''}")
-
     # Import the module for the day
-    day_module_name = f"days.{day}"
-    day_module = import_module(day_module_name)
+    day_module = import_module(f"days.{day}")
+
+    input_path = get_input_path(day, test, filename)
+
+    print(f"Running AOC day {day}{' IN TEST MODE' if test else ''} using {input_path}")
 
     # Read the raw input
-    input_path = get_input_path(filename, test, day)
-    print(f"Using input {input_path}")
     with open(input_path, "r") as file:
         blob = file.read()
 
@@ -42,14 +42,14 @@ def run(day, test, filename):
     t0 = datetime.now()
     answer_1 = day_module.do_part_1(processed_input)
     t1 = datetime.now()
-    print(f"Part 1: {answer_1} ({get_time_delta(t1, t0)}ms)")
 
     # Run part 2
     answer_2 = day_module.do_part_2(processed_input)
     t2 = datetime.now()
-    print(f"Part 2: {answer_2} ({get_time_delta(t2, t1)}ms)")
 
     # Finish
+    print(f"Part 1: {answer_1} ({get_time_delta(t1, t0)}ms)")
+    print(f"Part 2: {answer_2} ({get_time_delta(t2, t1)}ms)")
     print(f"Done in {get_time_delta(t2, t0)}ms")
 
 
