9dc6b4a772295fec3d846beea70df9ac3b3305f5
lam <lamnguyenhoang@gmail.com>
2022-12-02 09:51:35 +0100

    Day 2: improve naming

--- days/02/__init__.py
+++ days/02/__init__.py
@@ -1,4 +1,4 @@
-string_to_score = {
+PLAY_TO_SCORE = {
     "AX": 1 + 3,
     "AY": 2 + 6,
     "AZ": 3,
@@ -12,7 +12,7 @@ string_to_score = {
 
 # Translate a part-2 string to a part-1 desired play
 # X lose, Y draw, Z win
-string_to_play = {
+DESIRED_OUTCOME_TO_PLAY = {
     "AX": "AZ",
     "BX": "BX",
     "CX": "CY",
@@ -30,9 +30,9 @@ def process_input(blob):
 
 
 def do_part_1(processed_input):
-    return sum([string_to_score[text] for text in processed_input])
+    return sum([PLAY_TO_SCORE[play] for play in processed_input])
 
 
 def do_part_2(processed_input):
-    plays = [string_to_play[text] for text in processed_input]
-    return sum([string_to_score[text] for text in plays])
+    plays = [DESIRED_OUTCOME_TO_PLAY[outcome] for outcome in processed_input]
+    return sum([PLAY_TO_SCORE[play] for play in plays])
