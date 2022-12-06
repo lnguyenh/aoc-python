988611b75ed8ecfa23b09d90d44a535984259abc
lam <lamnguyenhoang@gmail.com>
2022-12-06 06:26:42 +0100

    Day 6: solve

--- days/06/__init__.py
+++ days/06/__init__.py
@@ -1,10 +1,35 @@
+from collections import deque
+from itertools import islice
+
+START_OF_PACKET_MARKER_LENGTH = 4
+START_OF_MESSAGE_MARKER_LENGTH = 14
+
+# Recipe from itertools https://docs.python.org/3/library/itertools.html
+def sliding_window(iterable, n):
+    # sliding_window('ABCDEFG', 4) --> ABCD BCDE CDEF DEFG
+    it = iter(iterable)
+    window = deque(islice(it, n), maxlen=n)
+    if len(window) == n:
+        yield tuple(window)
+    for x in it:
+        window.append(x)
+        yield tuple(window)
+
+
+def get_end_of_marker_index(datastream, marker_length):
+    for i, window in enumerate(sliding_window(datastream, marker_length)):
+        if len(set(window)) == marker_length:
+            return i + marker_length
+    return "error"
+
+
 def process_input(blob):
-    return blob.split("\n")
+    return blob
 
 
-def do_part_1(processed_input):
-    return "toto"
+def do_part_1(datastream):
+    return get_end_of_marker_index(datastream, START_OF_PACKET_MARKER_LENGTH)
 
 
-def do_part_2(processed_input):
-    return "toto"
+def do_part_2(datastream):
+    return get_end_of_marker_index(datastream, START_OF_MESSAGE_MARKER_LENGTH)
