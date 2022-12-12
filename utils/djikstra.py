119546affc61f76b93d0ce1137a63a38f9a34ae5
lam <lamnguyenhoang@gmail.com>
2022-12-12 08:52:11 +0100

    Day 12: renaming and comments

--- utils/djikstra.py
+++ utils/djikstra.py
@@ -45,27 +45,6 @@ def djikstra(edges, start, destination):
 
 if __name__ == "__main__":
     edges = [
-        ("A", "B", 7),
-        ("A", "D", 5),
-        ("B", "C", 8),
-        ("B", "D", 9),
-        ("B", "E", 7),
-        ("C", "E", 5),
-        ("D", "E", 15),
-        ("D", "F", 6),
-        ("E", "F", 8),
-        ("E", "G", 9),
-        ("F", "G", 11),
-    ]
-
-    # print("=== Dijkstra ===")
-    # print(edges)
-    # print("A -> E:")
-    # print(djikstra(edges, "A", "E"))
-    # print("F -> G:")
-    # print(djikstra(edges, "F", "G"))
-
-    edges = [
         ("A", "B", 1),
         ("A", "D", 99),
         ("B", "C", 2),
