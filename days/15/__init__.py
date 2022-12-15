1eae25d386ba3335b00263df4d8dec0f073a346c
lam <lamnguyenhoang@gmail.com>
2022-12-15 09:31:20 +0100

    Day 15: part 2

--- days/15/__init__.py
+++ days/15/__init__.py
@@ -1,3 +1,26 @@
+def simplify(intervals):
+    results = []
+    building = False
+    opened = set()
+    for i in sorted(intervals, key=lambda x: x[0]):
+        val, val_type, sensor_index = i
+
+        if val_type == "start":
+            opened.add(sensor_index)
+        else:
+            opened.remove(sensor_index)
+
+        if not building:
+            results.append(i)
+            building = True
+            continue
+        else:
+            if len(opened) == 0:
+                results.append(i)
+                building = False
+    return results
+
+
 class Point:
     def __init__(self, x, y):
         self.x = x
@@ -39,13 +62,16 @@ def process_input(blob):
 
 
 def do_part_1(sensors):
-    TARGET_Y = 2000000
+    if len(sensors) > 20:
+        TARGET_Y = 2000000
+    else:
+        TARGET_Y = 10
+
     forbidden = set()
     for i, sensor in enumerate(sensors):
         result = sensor.forbidden_xs(TARGET_Y)
         if result:
             x1, x2 = result
-            # print(f"{i} {x1} {x2}")
             for x in range(x1, x2 + 1):
                 forbidden.add(x)
     for i, sensor in enumerate(sensors):
@@ -54,5 +80,33 @@ def do_part_1(sensors):
     return len(forbidden)
 
 
-def do_part_2(processed_input):
-    return "toto"
+def do_part_2(sensors):
+    if len(sensors) > 20:
+        MAX = 4000000
+    else:
+        MAX = 20
+
+    result_y = None
+    result_x = None
+    for y in range(MAX + 1):
+        intervals = []
+        for i, sensor in enumerate(sensors):
+            result = sensor.forbidden_xs(y)
+            if result:
+                x1, x2 = result
+                intervals.append((x1, "start", i))
+                intervals.append((x2, "end", i))
+        simplified = simplify(intervals)
+        found = False
+        for i in range(len(simplified) - 1):
+            i1, i2 = simplified[i], simplified[i + 1]
+            if i2[0] - i1[0] == 2 and i1[1] == "end" and i2[1] == "start":
+                result_y = y
+                result_x = i1[0] + 1
+                if 0 <= result_x <= MAX:
+                    found = True
+                    break
+        if found:
+            break
+
+    return result_x * 4000000 + result_y
