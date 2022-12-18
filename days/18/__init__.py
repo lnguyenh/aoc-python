941fb5e735fe0c49b58474367d2aa72c113176de
lam <lamnguyenhoang@gmail.com>
2022-12-18 07:57:25 +0100

    Day 18: part 2

--- days/18/__init__.py
+++ days/18/__init__.py
@@ -6,7 +6,7 @@ def process_input(blob):
     coordinates = []
     for line in lines:
         x, y, z = line.split(",")
-        coordinates.append((x, y, z))
+        coordinates.append((int(x), int(y), int(z)))
     return coordinates
 
 
@@ -33,13 +33,13 @@ def do_part_1(coordinates):
     zs = defaultdict(list)
 
     for x, y, z in coordinates:
-        xs[(y, z)].append(int(x))
+        xs[(y, z)].append(x)
 
     for x, y, z in coordinates:
-        ys[(z, x)].append(int(y))
+        ys[(z, x)].append(y)
 
     for x, y, z in coordinates:
-        zs[(x, y)].append(int(z))
+        zs[(x, y)].append(z)
 
     for key in xs:
         xs[key] = sorted(xs[key])
@@ -54,5 +54,69 @@ def do_part_1(coordinates):
     return total
 
 
-def do_part_2(processed_input):
-    return "toto"
+def get_bubble(bubble, coordinates, checked, xmin, xmax, ymin, ymax, zmin, zmax):
+    could_expand = False
+    new_bubble = bubble.copy()
+    for x, y, z in bubble - checked:
+        expansions = [
+            (x + 1, y, z),
+            (x - 1, y, z),
+            (x, y - 1, z),
+            (x, y + 1, z),
+            (x, y, z - 1),
+            (x, y, z + 1),
+        ]
+        for x1, y1, z1 in expansions:
+            if not (
+                xmin - 1 <= x1 <= xmax + 1
+                and ymin - 1 <= y1 <= ymax + 1
+                and zmin - 1 <= z1 <= zmax + 1
+            ):
+                # out of bound. This is not a bubble
+                return None
+            if (x1, y1, z1) in coordinates:
+                # cannot expand here, there is a cube
+                continue
+            new_bubble.add((x1, y1, z1))
+            could_expand = True
+    if could_expand:
+        return get_bubble(
+            new_bubble, coordinates, bubble, xmin, xmax, ymin, ymax, zmin, zmax
+        )
+    else:
+        return bubble
+
+
+def do_part_2(coordinates):
+    x_list = [x for x, _, _ in coordinates]
+    y_list = [y for _, y, _ in coordinates]
+    z_list = [z for _, _, z in coordinates]
+    xmin = min(x_list)
+    ymin = min(y_list)
+    zmin = min(z_list)
+    xmax = max(x_list)
+    ymax = max(y_list)
+    zmax = max(z_list)
+
+    points_to_check = set()
+    for x in range(xmin, xmax + 1):
+        for y in range(ymin, ymax + 1):
+            for z in range(zmin, zmax + 1):
+                if (x, y, z) not in coordinates:
+                    points_to_check.add((x, y, z))
+
+    bubbles = set()
+    for x, y, z in points_to_check:
+        if any([(x, y, z) in b for b in bubbles]):
+            continue
+        bubble = get_bubble(
+            {(x, y, z)}, coordinates, set(), xmin, xmax, ymin, ymax, zmin, zmax
+        )
+        if bubble:
+            bubbles.add(frozenset(bubble))
+
+    for bubble in bubbles:
+        for x, y, z in bubble:
+            coordinates.append((x, y, z))
+
+    return do_part_1(coordinates)
