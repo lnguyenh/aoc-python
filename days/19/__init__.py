8f9fdd1f8154fd186b9f1d7d5b799ba63372407f
lam <lamnguyenhoang@gmail.com>
2022-12-19 23:20:49 +0100

    Day 19: cleanup

--- days/19/__init__.py
+++ days/19/__init__.py
@@ -63,6 +63,8 @@ class Factory:
 
         self.reached_end_once = False
 
+        # Kinda trying to optimize breaking out of the big recursion
+        # Not great optimization, but I think it helps some
         self.last_turn_to_get_one_obsidian_bot = None
         for i in range(len(PRODUCED) - 1):
             if PRODUCED[i] <= self.blueprint["geode"][1] < PRODUCED[i + 1]:
@@ -76,7 +78,6 @@ class Factory:
                 self.last_turn_to_get_one_clay_bot = (
                     self.last_turn_to_get_one_obsidian_bot - (i + 1) - 1
                 )
-        self.toto = 1
 
     def has_enough_for_ore_robot(self, resources):
         return self.blueprint["ore"] <= resources[0]
@@ -122,79 +123,6 @@ class Factory:
         return tuple(new_resources)
 
     @functools.cache
-    def find_best_path(self, robots, resources, i):
-
-        if i == self.num_turns:
-            if not self.reached_end_once:
-                self.reached_end_once = True
-            return resources[3]
-
-        # we receive our resources for this turn
-        new_resources = self.get_new_resources(robots, resources)
-
-        possibles = []
-
-        if self.has_enough_for_geode_robot(resources):  # based on old resources
-            new_robots = self.get_new_robots(robots, 3)
-            updated_new_resources = self.resources_post_purchase(new_resources, 3)
-            possibles.append((new_robots, updated_new_resources, i + 1))
-
-            if self.earliest_geode_bot_turn is None or i < self.earliest_geode_bot_turn:
-                self.earliest_geode_bot_turn = i
-
-        if self.has_enough_for_obsidian_robot(resources):  # based on old resources
-            new_robots = self.get_new_robots(robots, 2)
-            updated_new_resources = self.resources_post_purchase(new_resources, 2)
-            possibles.append((new_robots, updated_new_resources, i + 1))
-
-            if (
-                self.earliest_obsidian_bot_turn is None
-                or i < self.earliest_obsidian_bot_turn
-            ):
-                self.earliest_obsidian_bot_turn = i
-
-        if self.has_enough_for_clay_robot(resources):  # based on old resources
-            new_robots = self.get_new_robots(robots, 1)
-            updated_new_resources = self.resources_post_purchase(new_resources, 1)
-            possibles.append((new_robots, updated_new_resources, i + 1))
-
-            if self.earliest_clay_bot_turn is None or i < self.earliest_clay_bot_turn:
-                self.earliest_clay_bot_turn = i
-
-        if self.has_enough_for_ore_robot(resources):  # based on old resources
-            new_robots = self.get_new_robots(robots, 0)
-            updated_new_resources = self.resources_post_purchase(new_resources, 0)
-            possibles.append((new_robots, updated_new_resources, i + 1))
-
-        new_robots = robots[:]
-        possibles.append((new_robots, new_resources, i + 1))
-
-        for possible in possibles:
-            # try to break out as early as possible
-            if (
-                (
-                    possible[0][3] == 0
-                    and self.earliest_geode_bot_turn
-                    and i + 1 > self.earliest_geode_bot_turn
-                )
-                or (
-                    possible[0][2] == 0
-                    and self.earliest_obsidian_bot_turn
-                    and i + 1 > self.earliest_obsidian_bot_turn
-                )
-                or (
-                    possible[0][2] == 0
-                    and i + 1 >= self.last_turn_to_get_one_obsidian_bot
-                )
-                or (possible[0][1] == 0 and i + 1 >= self.last_turn_to_get_one_clay_bot)
-                or ((i + 1) >= self.num_turns - 1 and possible[0][3] == 0)
-            ):
-                continue
-            num_geodes = self.find_best_path(*possible)
-            if num_geodes > self.current_best:
-                self.current_best = num_geodes
-        return self.current_best
-
     def hope(self, i, num_geodes, num_robots):
         num_geodes_total = num_geodes
         num_robots_total = num_robots
@@ -204,7 +132,7 @@ class Factory:
         return num_geodes_total
 
     @functools.cache
-    def find_best_path_2(self, robots, resources, i):
+    def find_best_path(self, robots, resources, i):
 
         if i == self.num_turns:
             if not self.reached_end_once:
@@ -222,14 +150,8 @@ class Factory:
             and self.blueprint["geode"][1] <= new_resources[2]
             and self.blueprint["geode"][0] <= new_resources[0]
         )
-        # if self.go_in_geode_robot_mode_turn is None and go_in_geode_robot_mode) or (
-        #     self.go_in_geode_robot_mode_turn and i < self.go_in_geode_robot_mode_turn
-        # ):
         if go_in_geode_robot_mode:
             self.go_in_geode_robot_mode_turn = i
-            self.num_geodes_when_going_mode = resources[3]
-            x = self.hope(i, resources[3], robots[3])
-            return x
 
         if go_in_geode_robot_mode or self.has_enough_for_geode_robot(
             resources
@@ -277,15 +199,9 @@ class Factory:
 
         for possible in possibles:
             # try to break out as early as possible
-
             if (
                 (
-                    possible[0][3] == 0
-                    and self.earliest_geode_bot_turn
-                    and i + 1 > self.earliest_geode_bot_turn
-                )
-                or (
-                    self.go_in_geode_robot_mode_turn is not None
+                    self.current_best is not None
                     and self.hope(i + 1, possible[1][3], possible[0][3])
                     < self.current_best
                 )
@@ -297,22 +213,19 @@ class Factory:
                 or ((i + 1) >= self.num_turns - 1 and possible[0][3] == 0)
             ):
                 continue
-            num_geodes = self.find_best_path_2(*possible)
+            num_geodes = self.find_best_path(*possible)
             if num_geodes > self.current_best:
                 self.current_best = num_geodes
         return self.current_best
 
 
 def do_part_1(blueprints):
-    return None
     geodes = []
     for name, blueprint in blueprints:
         factory = Factory(name, blueprint, 24)
         m = factory.find_best_path((1, 0, 0, 0), (0, 0, 0, 0), 0)
         print(f"{factory.name} - max: {m}")
         geodes.append((m, int(factory.name)))
-    print(geodes)
-
     return sum([a * b for a, b in geodes])
 
 
@@ -321,9 +234,8 @@ def do_part_2(blueprints):
     maxes = []
     for name, blueprint in blueprints[:3]:
         factory = Factory(name, blueprint, 32)
-        m = factory.find_best_path_2((1, 0, 0, 0), (0, 0, 0, 0), 0)
+        m = factory.find_best_path((1, 0, 0, 0), (0, 0, 0, 0), 0)
         print(f"{factory.name} - max: {m}")
         geodes.append((m, int(factory.name)))
         maxes.append(m)
-    print(geodes)
     return functools.reduce(lambda x, y: x * y, maxes)
