25e7f8d28bc15ef528dfb5586117d2292198043b
lam <lamnguyenhoang@gmail.com>
2022-12-20 19:51:42 +0100

    Day 20: part 1

--- days/20/__init__.py
+++ days/20/__init__.py
@@ -1,103 +1,106 @@
-from collections import deque
-
-
-class Number:
-    def __init__(self, number, position):
-        self.name = chr(ord("a") + position)
+class Node:
+    def __init__(self, number, i):
+        self.i = i
         self.value = number
+        self.n = None
+        self.p = None
 
     def __str__(self):
         return f"{self.value}"
 
     def __repr__(self):
-        return f"{self.value} ({self.name})"
+        return f"{self.value}"
 
 
 def process_input(blob):
-    return [Number(int(x), i) for i, x in enumerate(blob.split("\n"))]
-
-
-def move_one_step_right(name, deck):
-    # pop everything until we have n in our hands
-    popped = deque()
-    n = None
-    while True:
-        n = deck.popleft()
-        if n.name == name:
-            break
-        popped.append(n)
-
-    if not n:
-        raise Exception("toti")
-
-    if not deck:
-        while popped:
-            deck.appendleft(popped.pop())
-        popped.append(deck.popleft())
-        deck.appendleft(n)
-        deck.appendleft(popped.pop())
-    else:
-        popped.append(deck.popleft())
-        deck.appendleft(n)
-        while popped:
-            deck.appendleft(popped.pop())
-    return deck
-
-
-def move_one_step_left(name, deck):
-    # pop everything until we have n in our hands
-    popped = deque()
-    n = None
-    while True:
-        n = deck.popleft()
-        if n.name == name:
-            break
-        popped.append(n)
-
-    if not n:
-        raise Exception("toto")
-
-    if not popped:
-        popped.append(deck.pop())
-        deck.append(n)
-        deck.append(popped.pop())
-    else:
-        deck.appendleft(popped.pop())
-        if not popped:
-            deck.append(n)
+    nodes = [Node(int(x), i) for i, x in enumerate(blob.split("\n"))]
+    for i, node in enumerate(nodes):
+        if i == 0:
+            node.p = len(nodes) - 1
+            node.n = i + 1
+        elif i == len(nodes) - 1:
+            node.n = 0
+            node.p = i - 1
         else:
-            deck.appendleft(n)
-            while popped:
-                deck.appendleft(popped.pop())
-    return deck
+            node.n = i + 1
+            node.p = i - 1
+    return nodes
 
 
-def do_part_1(numbers):
-    deck = deque(numbers)
-    names = [n.name for n in numbers]
-    as_dict = {n.name: n for n in numbers}
+def find_target(node, nodes):
+    steps = node.value
+    target_node = node
+    if steps > 0:
+        for _ in range(steps):
+            target_node = nodes[target_node.n]
+            if target_node.i == node.i:
+                target_node = nodes[target_node.n]
+    if steps < 0:
+        for _ in range(-steps + 1):
+            target_node = nodes[target_node.p]
+            if target_node.i == node.i:
+                target_node = nodes[target_node.p]
+    return target_node
 
-    for name in names:
-        n = as_dict[name]
-        steps = n.value
-        if steps > 0:
-            for _ in range(steps):
-                move_one_step_right(name, deck)
-                toto = 0
-        else:
-            for _ in range(-steps):
-                move_one_step_left(name, deck)
-                toto = 0
-    i = 0
-    for n in deck:
-        if n.value == 0:
+
+def unlink_node(node, nodes):
+    previous_node = nodes[node.p]
+    next_node = nodes[node.n]
+    previous_node.n = next_node.i
+    next_node.p = previous_node.i
+    return node
+
+
+def print_nodes(nodes):
+    current_node = None
+    for node in nodes:
+        if node.value == 0:
+            current_node = node
             break
-        i += 1
-    th1000 = (i + 1000) % len(deck)
-    th2000 = (i + 2000) % len(deck)
-    th3000 = (i + 3000) % len(deck)
-    a, b, c = deck[th1000].value, deck[th2000].value, deck[th3000].value
-    return a + b + c
+    output = []
+    for _ in range(len(nodes) * 2):
+        output.append(current_node.value)
+        current_node = nodes[current_node.n]
+    print(output)
+
+
+def insert_after(node, target_node, nodes):
+    next_node = nodes[target_node.n]
+
+    target_node.n = node.i
+    node.p = target_node.i
+    node.n = next_node.i
+    next_node.p = node.i
+
+
+def move(nodes, node, steps):
+    current = node
+    for _ in range(steps):
+        current = nodes[current.n]
+    return current
+
+
+def do_part_1(nodes):
+    print_nodes(nodes)
+    for node in nodes:
+        target_node = find_target(node, nodes)
+        if target_node.n != node.i and node.value != 0:
+            unlink_node(node, nodes)
+            insert_after(node, target_node, nodes)
+
+    zero_node = None
+    for node in nodes:
+        if node.value == 0:
+            zero_node = node
+            break
+    th1000 = move(nodes, zero_node, 1000)
+    th2000 = move(nodes, th1000, 1000)
+    th3000 = move(nodes, th2000, 1000)
+    v1, v2, v3 = th1000.value, th2000.value, th3000.value
+    print(v1, v2, v3)
+    return v1 + v2 + v3
+
+    return "toto"
 
 
 def do_part_2(processed_input):
