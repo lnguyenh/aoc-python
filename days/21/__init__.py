4949cde3919c2d071d3973ea676c165df495194f
lam <lamnguyenhoang@gmail.com>
2022-12-21 07:47:31 +0100

    Day 21: too slow part 2

--- days/21/__init__.py
+++ days/21/__init__.py
@@ -1,3 +1,6 @@
+from math import floor
+
+
 class Monkey:
     def __init__(self, name, text):
         self.name = name
@@ -26,29 +29,59 @@ class Monkey:
             self.number = self.av / self.bv
         elif self.o == "-":
             self.number = self.av - self.bv
+        return self.number
 
 
 def process_input(blob):
+    return blob
+
+
+def get_monkeys(blob, humn=None):
     lines = blob.split("\n")
     monkeys = {}
     for line in lines:
         name, text = line.split(": ")
         monkeys[name] = Monkey(name, text)
+    if humn is not None:
+        monkeys["humn"].number = humn
+
     return monkeys
 
 
-def do_part_1(monkeys):
-    while monkeys["root"].number is None:
-        for name, m in monkeys.items():
-            if not m.number:
-                if not m.av:
-                    m.av = monkeys.get(m.a).number
-                if not m.bv:
-                    m.bv = monkeys.get(m.b).number
-            if m.can_solve():
-                m.solve()
-    return int(monkeys["root"].number)
+def do_part_1(blob):
+    return None
+    # monkeys = get_monkeys(blob)
+    # while monkeys["root"].number is None:
+    #     for name, m in monkeys.items():
+    #         if not m.number:
+    #             if not m.av:
+    #                 m.av = monkeys.get(m.a).number
+    #             if not m.bv:
+    #                 m.bv = monkeys.get(m.b).number
+    #         if m.can_solve():
+    #             m.solve()
+    # return int(monkeys["root"].number)
 
 
-def do_part_2(monkeys):
-    return "toto"
+def do_part_2(blob):
+    i = 0
+    while True:
+        monkeys = get_monkeys(blob, humn=i)
+        abort = False
+        while not monkeys["root"].can_solve() and not abort:
+            for name, m in monkeys.items():
+                if m.number is None:
+                    if not m.av:
+                        m.av = monkeys.get(m.a).number
+                    if not m.bv:
+                        m.bv = monkeys.get(m.b).number
+                if m.can_solve():
+                    r = m.solve()
+                    if floor(r) != r:
+                        abort = True
+                        break
+        if monkeys["root"].can_solve() and monkeys["root"].av == monkeys["root"].bv:
+            break
+        i += 1
+
+    return i
