7f8a53aca568bb7fd0d36b5ab30b5a8910ddd3f8
lam <lamnguyenhoang@gmail.com>
2022-12-21 06:24:44 +0100

    Day 21: ppart 1

--- days/21/__init__.py
+++ days/21/__init__.py
@@ -1,10 +1,54 @@
+class Monkey:
+    def __init__(self, name, text):
+        self.name = name
+        self.number = None
+        self.a = None
+        self.av = None
+        self.b = None
+        self.bv = None
+        self.o = None
+
+        elements = text.split(" ")
+        if len(elements) == 1:
+            self.number = int(elements[0])
+        else:
+            self.a, self.o, self.b = elements
+
+    def can_solve(self):
+        return self.av is not None and self.bv is not None
+
+    def solve(self):
+        if self.o == "+":
+            self.number = self.av + self.bv
+        elif self.o == "*":
+            self.number = self.av * self.bv
+        elif self.o == "/":
+            self.number = self.av / self.bv
+        elif self.o == "-":
+            self.number = self.av - self.bv
+
+
 def process_input(blob):
-    return blob.split("\n")
+    lines = blob.split("\n")
+    monkeys = {}
+    for line in lines:
+        name, text = line.split(": ")
+        monkeys[name] = Monkey(name, text)
+    return monkeys
 
 
-def do_part_1(processed_input):
-    return "toto"
+def do_part_1(monkeys):
+    while monkeys["root"].number is None:
+        for name, m in monkeys.items():
+            if not m.number:
+                if not m.av:
+                    m.av = monkeys.get(m.a).number
+                if not m.bv:
+                    m.bv = monkeys.get(m.b).number
+            if m.can_solve():
+                m.solve()
+    return int(monkeys["root"].number)
 
 
-def do_part_2(processed_input):
+def do_part_2(monkeys):
     return "toto"
