070bda61c3c1dea0838f36b900349a4ee8368a32
lam <lamnguyenhoang@gmail.com>
2022-12-22 15:29:28 +0100

    Day 22: wip part 2 running but wrong

--- days/22/__init__.py
+++ days/22/__init__.py
@@ -11,11 +11,15 @@ class MonkeyMap:
         self.max_i = 0
         self.max_j = 0
 
+        self.w = 6000
+
+        self.real_direction = 0  # 0: right, 1: down, 2: left, 3: up
         self.direction = 0  # 0: right, 1: down, 2: left, 3: up
         self.populate_grid(text)
 
     def populate_grid(self, text):
         for j, line in enumerate(text.split("\n")):
+            self.w = min([self.w, len(line.replace(" ", ""))])
             self.max_j = j
             for i, c in enumerate(line):
                 self.max_i = max(i, self.max_i)
@@ -28,33 +32,113 @@ class MonkeyMap:
     def move(self, steps):
         next_position = [(1, 0), (0, 1), (-1, 0), (0, -1)]
         for _ in range(steps):
-            self.position = self.get_next_position(next_position[self.direction])
+            self.position = self.get_next_position(next_position[self.real_direction])
             toti = 1
 
-    def get_next_position(self, vector):
-        candidate = (self.position[0] + vector[0], self.position[1] + vector[1])
-        while True:
-            if self.grid.get(candidate):
-                if self.grid[candidate] == "#":
-                    return self.position
-                else:
-                    return candidate
-            candidate = (candidate[0] + vector[0], candidate[1] + vector[1])
-            if candidate[0] > self.max_i:
-                candidate = (0, candidate[1])
-            elif candidate[0] < 0:
-                candidate = (self.max_i, candidate[1])
+    # def get_next_position(self, vector):
+    #     candidate = (self.position[0] + vector[0], self.position[1] + vector[1])
+    #     while True:
+    #         if self.grid.get(candidate):
+    #             if self.grid[candidate] == "#":
+    #                 return self.position
+    #             else:
+    #                 return candidate
+    #         candidate = (candidate[0] + vector[0], candidate[1] + vector[1])
+    #         if candidate[0] > self.max_i:
+    #             candidate = (0, candidate[1])
+    #         elif candidate[0] < 0:
+    #             candidate = (self.max_i, candidate[1])
+    #
+    #         if candidate[1] > self.max_j:
+    #             candidate = (candidate[0], 0)
+    #         elif candidate[1] < 0:
+    #             candidate = (candidate[0], self.max_j)
 
-            if candidate[1] > self.max_j:
-                candidate = (candidate[0], 0)
-            elif candidate[1] < 0:
-                candidate = (candidate[0], self.max_j)
+    def get_next_position(self, vector):
+        w = self.w
+        old_x, old_y = self.position
+        saved_real_direction = self.real_direction
+
+        candidate = (self.position[0] + vector[0], self.position[1] + vector[1])
+        x, y = candidate
+
+        if not self.grid.get(candidate):
+            if x <= (2 * w - 1) and y == -1:
+                # A
+                self.rotate_real(1)
+                candidate = (0, x + 2 * w)
+            elif x == -1 and y >= 3 * w:
+                # A'
+                self.rotate(3)
+                candidate = (y - 2 * w, 0)
+            elif y == 2 * w - 1 and old_y == 2 * w:
+                # B
+                self.rotate_real(1)
+                candidate = (w, (2 * w - 1) - ((w - 1) - x))
+            elif x == w - 1 and y >= w:
+                # B'
+                self.rotate_real(3)
+                candidate = ((w - 1) - ((2 * w - 1) - y), 2 * w)
+            elif x == 2 * w and y < (2 * w):
+                # C
+                self.rotate_real(3)
+                candidate = (y + w, w - 1)
+            elif y == w and old_y == (w - 1):
+                # C'
+                self.rotate_real(1)
+                candidate = (2 * w - 1, x - w)
+            elif x == w and old_x == w - 1:
+                # D
+                self.rotate_real(3)
+                candidate = (y - 2 * w, 3 * w - 1)
+            elif y == 3 * w and old_y == 3 * w - 1:
+                # D'
+                self.rotate_real(1)
+                candidate = (w - 1, 2 * w + x)
+            elif x == w - 1 and y <= (w - 1):
+                # E
+                self.rotate_real(2)
+                candidate = (0, 2 * w + (w - 1 - y))
+            elif x == -1 and y < 3 * w:
+                # E'
+                self.rotate_real(2)
+                candidate = (w, (w - 1) - (y - 2 * w))
+            elif x == 2 * w and y >= 2 * w:
+                # F
+                self.rotate_real(2)
+                candidate = (3 * w - 1, (w - 1) - (y - 2 * w))
+            elif x == 3 * w:
+                # F'
+                self.rotate_real(2)
+                candidate = (2 * w - 1, 2 * w + (w - 1 - y))
+            elif y == -1 and x >= 2 * w:
+                # G
+                candidate = (x - 2 * w, 4 * w - 1)
+            elif y >= 4 * w:
+                # G'
+                candidate = (2 * w + x, 0)
+            else:
+                toto = 1
+
+        print(f"{candidate} - old {old_x}/{old_y} - new {x}{y}")
+
+        if self.grid[candidate] == "#":
+            self.real_direction = saved_real_direction
+            return self.position
+        else:
+            return candidate
 
     def rotate(self, letter):
         if letter == "R":
             self.direction = (self.direction + 1) % 4
+            self.real_direction = (self.real_direction + 1) % 4
         else:
             self.direction = (self.direction - 1) % 4
+            self.real_direction = (self.real_direction - 1) % 4
+
+    def rotate_real(self, num):
+        for _ in range(num):
+            self.real_direction = (self.real_direction + 1) % 4
 
     def run(self):
         for instr in self.instructions:
