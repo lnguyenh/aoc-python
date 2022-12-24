58902a68e4cadc190db59640246e3f5d32fbbcbe
lam <lamnguyenhoang@gmail.com>
2022-12-24 23:24:04 +0100

    Add bfs shortest path implementation

---
+++ utils/bfs.py
@@ -0,0 +1,59 @@
+from collections import defaultdict, deque
+
+
+def bfs(edges, start, destination):
+    """
+    Gets shortest path from start to destination using BFS
+    """
+
+    # neighbours / graph
+    neighbours = defaultdict(list)
+    for from_node, to_node in edges:
+        neighbours[from_node].append(to_node)
+
+    # unique nodes
+    nodes = set()
+    for n1, n2 in edges:
+        nodes.add(n1)
+        nodes.add(n2)
+
+    # visited marker
+    visited = {node: False for node in nodes}
+
+    visited[start] = True
+    paths = deque([[start]])
+
+    if start == destination:
+        return 0, [start]
+
+    while paths:
+        path = paths.pop()
+        node = path[-1]
+
+        for neighbour in neighbours[node]:
+            if visited[neighbour]:
+                continue
+            new_path = list(path)
+            new_path.append(neighbour)
+
+            if neighbour == destination:
+                return len(new_path), new_path
+
+            paths.append(new_path)
+            visited[node] = True
+
+    return float("inf"), None
+
+
+if __name__ == "__main__":
+    edges = [
+        ("A", "B"),
+        ("B", "C"),
+        ("C", "F"),
+        ("C", "E"),
+        ("C", "G"),
+        ("G", "H"),
+        ("E", "D"),
+    ]
+    print("A -> D:")
+    print(bfs(edges, "A", "D"))
