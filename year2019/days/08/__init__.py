a621ba68bb16ec88ca2d3b7cdbfc871061d714c4
lam <lamnguyenhoang@gmail.com>
2022-12-25 22:29:27 +0100

    201908: part 2

--- year2019/days/08/__init__.py
+++ year2019/days/08/__init__.py
@@ -7,15 +7,24 @@ def do_part_1(text):
     result = None
     for chunk in zip(*(iter(text),) * (25 * 6)):
         num_zeros = chunk.count("0")
-        num_ones = chunk.count("1")
-        num_twos = chunk.count("2")
         if min_num_zeros is None or num_zeros < min_num_zeros:
             min_num_zeros = num_zeros
-            result = num_ones * num_twos
+            result = chunk.count("1") * chunk.count("2")
     return result
 
 
-def do_part_2(processed_input):
+def do_part_2(text):
+    chunks = list(zip(*(iter(text),) * (25 * 6)))
+
+    image = []
+    for i in range(len(chunks[0])):
+        for j in range(len(chunks)):
+            if chunks[j][i] != "2":
+                image.append(chunks[j][i])
+                break
+
+    for chunk in zip(*(iter(image),) * 25):
+        print("".join(chunk).replace("1", "*").replace("0", " "))
     return "toto"
 
 
