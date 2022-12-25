5104e6b41ea0c0c1d54492e58c0da9474e22b217
lam <lamnguyenhoang@gmail.com>
2022-12-25 14:38:20 +0100

    201905: part 1

--- 2019/days/05/__init__.py
+++ 2019/days/05/__init__.py
@@ -1,30 +1,65 @@
+from utils.regexp import search_groups
+
+
 class IntCode:
     def __init__(self, program):
         self.p = program[:]
         self.original_program = program
 
-    def run_one(self, i):
-        instr = self.p[i]
+    def parse_instruction(self, i):
+        text = str(self.p[i]).rjust(5, "0")
+        a, b, c, d = search_groups(r"(\d)?(\d)?(\d)?(\d?\d)$", text)
+        return int(a), int(b), int(c), d
 
-        if instr == 1:
-            a, b, c = self.p[i + 1], self.p[i + 2], self.p[i + 3]
-            self.p[c] = self.p[a] + self.p[b]
-            return i + 4
-        elif instr == 2:
-            a, b, c = self.p[i + 1], self.p[i + 2], self.p[i + 3]
-            self.p[c] = self.p[a] * self.p[b]
-            return i + 4
-        elif instr == 3:
+    def run_one(self, i):
+        mode_1, mode_2, mode_3, code = self.parse_instruction(i)
+        modes = mode_1, mode_2, mode_3
+        instructions = {
+            "01": self.add,
+            "02": self.multiply,
+            "03": self.input,
+            "04": self.output,
+            "99": self.finish,
+        }
+        return instructions[code](i, modes)
+
+    def get_value(self, i, mode):
+        content = self.p[i]
+        return content if mode else self.p[content]
+
+    def add(self, i, modes):
+        a = self.get_value((i + 1), modes[2])
+        b = self.get_value((i + 2), modes[1])
+        total = a + b
+        c = self.p[i + 3]
+        self.p[c] = total
+        return i + 4
+
+    def multiply(self, i, modes):
+        a = self.get_value(i + 1, modes[2])
+        b = self.get_value(i + 2, modes[1])
+        total = a * b
+        c = self.p[i + 3]
+        self.p[c] = total
+        return i + 4
+
+    def input(self, i, modes):
+        print("Type input value:")
+        value = input()
+        a = self.p[i + 1]
+        self.p[a] = int(value)
+        return i + 2
+
+    def output(self, i, modes):
+        if modes[2]:
+            print(f"{self.p[1 + 1]}")
+        else:
             a = self.p[i + 1]
-            print("Type input value:")
-            value = input()
-            self.p[a] = value
-            return i + 2
-        elif instr == 4:
             print(f"{self.p[a]}")
-            return i + 2
-        elif instr == 99:
-            return None
+        return i + 2
+
+    def finish(self, i, modes):
+        return None
 
     def run(self):
         i = 0
@@ -32,9 +67,6 @@ class IntCode:
             i = self.run_one(i)
             if i is None:
                 break
-
-    def get_value(self, index):
-        return self.p[index]
 
     def set(self, value, index):
         self.p[index] = value
@@ -44,10 +76,12 @@ class IntCode:
 
 
 def process_input(blob):
-    return blob.split("\n")
+    return [int(n) for n in blob.split(",")]
 
 
-def do_part_1(processed_input):
+def do_part_1(program):
+    intcode = IntCode(program)
+    intcode.run()
     return "toto"
 
 
---
+++ utils/regexp.py
@@ -0,0 +1,66 @@
+import re
+
+
+def findall(pattern, text):
+    """
+    Finds all occurrences of a pattern
+    Does not show named groups
+    Returns a list
+    """
+    reg = re.compile(pattern)
+    return reg.findall(text)
+
+
+def search_named_groups(pattern, text):
+    reg = re.compile(pattern)
+    match = reg.search(text)
+    if match:
+        return match.groupdict()
+    return None
+
+
+def search_groups(pattern, text):
+    reg = re.compile(pattern)
+    match = reg.search(text)
+    if match:
+        return match.groups()
+    return None
+
+
+"""
+match vs search
+
+re.match() function of re in Python will search the regular expression pattern and 
+return the first occurrence. The Python RegEx Match method checks for a match only at 
+the beginning of the string. So, if a match is found in the first line, it returns the 
+match object. But if a match is found in some other line, the Python RegEx Match 
+function returns null.
+
+re.search() function will search the regular expression pattern and return the first 
+occurrence. Unlike Python re.match(), it will check all lines of the input string. The 
+Python re.search() function returns a match object when the pattern is found and “null” 
+if the pattern is not found
+
+"""
+
+if __name__ == "__main__":
+    # Findall examples
+    print(findall("([0-9]+)([a-zA-Z]?)", "10R5L5R10L4R5L5"))
+    print(findall("toto", "toto mange toto"))
+    print(findall(r"(?P<word>\b\w+\b)", "(((( Lots of punctuation )))"))
+
+    # Named group search
+    print(search_named_groups(r"(?P<first>\w+) (?P<last>\w+)", "Jane Doe"))
+    print(search_named_groups(r"(?P<first>\w+) (?P<last>\w+)", "1234"))
+    print(
+        search_named_groups(
+            r"(?P<mode1>\d)?(?P<mode2>\d)(?P<mode3>\d)(?P<opcode>\d\d)", "1002"
+        )
+    )
+
+    # Group by index search
+    print(
+        search_groups(
+            r"(?P<mode1>\d)?(?P<mode2>\d)(?P<mode3>\d)(?P<opcode>\d\d)", "1002"
+        )
+    )
