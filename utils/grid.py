a085fa302e74819c480f99614922c466504aa46d
lam <lamnguyenhoang@gmail.com>
2023-05-09 08:20:31 +0200

    Restore the old Point class

--- utils/grid.py
+++ utils/grid.py
@@ -7,6 +7,15 @@ class Point:
         self.x = x
         self.y = y
 
+    def __str__(self):
+        return f"x={self.x}, y={self.y}"
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
+    def __hash__(self):
+        return hash(tuple((self.x, self.y)))
+
 
 class Grid:
 
