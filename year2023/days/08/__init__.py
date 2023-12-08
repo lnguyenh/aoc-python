430cd423db38ac39457c25f7522e16d4f9084be3
Lam <lamnguyenhoang@gmail.com>
2023-12-08 08:11:45 +0100

    Day 8 part 2 wip

--- year2023/days/08/__init__.py
+++ year2023/days/08/__init__.py
@@ -1,4 +1,5 @@
 from itertools import cycle
+from math import lcm
 
 
 def process_input(blob):
@@ -19,7 +20,6 @@ def process_input(blob):
 
 
 def do_part_1(processed_input):
-    return "toto"
     instructions, patterns, _ = processed_input
     infinite_instructions = cycle(instructions)
     node = "AAA"
@@ -31,27 +31,34 @@ def do_part_1(processed_input):
     return count
 
 
-def is_done_part_2(positions):
-    return all([position.endswith("Z") for _, position in positions.items()])
-
-
 def do_part_2(processed_input):
     instructions, patterns, nodes = processed_input
-    infinite_instructions = cycle(instructions)
-    starts_nodes = [node for node in nodes if node.endswith("A")]
+    a_nodes = [node for node in nodes if node.endswith("A")]
 
-    positions = {node: node for node in starts_nodes}
-    count = 0
-    while not is_done_part_2(positions):
-        lr = next(infinite_instructions)
+    jumps = {}
+    for node in nodes:
+        infinite_instructions = cycle(instructions)
 
-        new_positions = {}
-        for node, position in positions.items():
-            new_positions[node] = patterns[(position, lr)]
-        positions = new_positions
-        count += 1
-    return count
+        position = node
+        count = 0
+        while True:
+            lr = next(infinite_instructions)
 
+            # Break for dead nodes like XXX = (XXX, XXX)
+            if patterns[(position, "R")] == patterns[(position, "L")] == position:
+                break
 
-def do_visualization(processed_input):
-    return None
+            position = patterns[(position, lr)]
+            count += 1
+
+            if position.endswith("Z") or position == node:
+                break
+        jumps[node] = (count, position)
+
+    useful_jumps = {
+        node: value for node, value in jumps.items() if value[1].endswith("Z")
+    }
+
+    coefficients = [useful_jumps[node][0] for node in a_nodes]
+
+    return lcm(*coefficients)
