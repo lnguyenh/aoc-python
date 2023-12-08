d3cffbccfffc0a2f283c3736fa7d6f9581461ccc
Lam <lamnguyenhoang@gmail.com>
2023-12-08 06:19:47 +0100

    Day 8 part 1

--- year2023/days/08/__init__.py
+++ year2023/days/08/__init__.py
@@ -1,9 +1,33 @@
+from itertools import cycle
+
+
 def process_input(blob):
-    return blob.split("\n")
+    instructions, raw_patterns = blob.split("\n\n")
+
+    patterns = {}
+    for raw_pattern in raw_patterns.split("\n"):
+        tmp = raw_pattern.replace(" = (", ":")
+        tmp = tmp.replace(", ", ":")
+        tmp = tmp.replace(")", "")
+        origin, left, right = tmp.split(":")
+        patterns[(origin, "L")] = left
+        patterns[(origin, "R")] = right
+
+    return instructions, patterns
 
 
 def do_part_1(processed_input):
-    return "toto"
+    instructions, patterns = processed_input
+    infinite_instructions = cycle(instructions)
+
+    node = "AAA"
+    count = 0
+    while node != "ZZZ":
+        lr = next(infinite_instructions)
+        node = patterns[(node, lr)]
+        count += 1
+
+    return count
 
 
 def do_part_2(processed_input):
