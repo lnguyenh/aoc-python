fae5e220669de8f74d6a43ce25273f6f6dc4fc55
Lam <lamnguyenhoang@gmail.com>
2023-12-14 07:27:03 +0100

    Day 14 part 2

--- year2023/days/14/__init__.py
+++ year2023/days/14/__init__.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 from utils.grid import Grid
 
 
@@ -6,6 +8,7 @@ class Platform(Grid):
 
     def __init__(self, lines):
         super().__init__(lines)
+        self.states = defaultdict(list)
 
     def tilt_north(self):
         moved = True
@@ -16,10 +19,64 @@ class Platform(Grid):
                     c = self.grid.get((i, j))
                     if c == "O":
                         tx, ty = i, j - 1
-                        if j - 1 >= 0 and (tx, ty) not in self.grid:
+                        if ty >= 0 and (tx, ty) not in self.grid:
                             self.grid[(tx, ty)] = c
                             self.grid.pop((i, j))
                             moved = True
+
+    def tilt_south(self):
+        moved = True
+        while moved:
+            moved = False
+            for j in range(self.maxy, self.miny - 1, -1):
+                for i in range(self.maxx + 1):
+                    c = self.grid.get((i, j))
+                    if c == "O":
+                        tx, ty = i, j + 1
+                        if ty <= self.maxy and (tx, ty) not in self.grid:
+                            self.grid[(tx, ty)] = c
+                            self.grid.pop((i, j))
+                            moved = True
+
+    def tilt_west(self):
+        moved = True
+        while moved:
+            moved = False
+            for i in range(self.maxx + 1):
+                for j in range(self.maxy + 1):
+                    c = self.grid.get((i, j))
+                    if c == "O":
+                        tx, ty = i - 1, j
+                        if tx >= 0 and (tx, ty) not in self.grid:
+                            self.grid[(tx, ty)] = c
+                            self.grid.pop((i, j))
+                            moved = True
+
+    def tilt_east(self):
+        moved = True
+        while moved:
+            moved = False
+            for i in range(self.maxx + 1, -1, -1):
+                for j in range(self.maxy + 1):
+                    c = self.grid.get((i, j))
+                    if c == "O":
+                        tx, ty = i + 1, j
+                        if tx <= self.maxx and (tx, ty) not in self.grid:
+                            self.grid[(tx, ty)] = c
+                            self.grid.pop((i, j))
+                            moved = True
+
+    def do_one_cycle(self):
+        self.tilt_north()
+        self.tilt_west()
+        self.tilt_south()
+        self.tilt_east()
+
+    def save_state(self, i):
+        state = tuple(self.grid.keys())
+        # print(state)
+        self.states[state].append(i)
+        return self.states[state]
 
     def get_weight(self):
         weights = []
@@ -38,8 +95,18 @@ def do_part_1(platform):
     return platform.get_weight()
 
 
-def do_part_2(processed_input):
-    return "toto"
+def do_part_2(platform):
+    i = 0
+    while True:
+        platform.do_one_cycle()
+        x = platform.save_state(i)
+        i += 1
+        if len(x) == 2:
+            cycle_length = x[1] - x[0]
+            num_cycles_left = (999999999 - x[0]) % cycle_length
+            for _ in range(num_cycles_left):
+                platform.do_one_cycle()
+            return platform.get_weight()
 
 
 def do_visualization(processed_input):
