84d384488f45c40227f96337ad8e29f9aa9bec3d
Lam <lamnguyenhoang@gmail.com>
2023-12-15 07:32:40 +0100

    Day 15 part 2

--- year2023/days/15/__init__.py
+++ year2023/days/15/__init__.py
@@ -1,22 +1,47 @@
+from collections import OrderedDict
+from functools import lru_cache
+
+
 def process_input(blob):
     return blob.split(",")
+
+
+@lru_cache
+def get_hash(word):
+    cv = 0
+    for c in word:
+        cv += ord(c)
+        cv *= 17
+        cv = cv % 256
+    return cv
 
 
 def do_part_1(processed_input):
     hashes = []
     for word in processed_input:
-        cv = 0
-        for c in word:
-            cv += ord(c)
-            cv *= 17
-            cv = cv % 256
-        hashes.append(cv)
+        hashes.append(get_hash(word))
     return sum(hashes)
 
 
 def do_part_2(processed_input):
-    return "toto"
 
+    boxes = []
+    for _ in range(256):
+        boxes.append(OrderedDict())
 
-def do_visualization(processed_input):
-    return None
+    for word in processed_input:
+        if word.endswith("-"):
+            label = word[:-1]
+            h = get_hash(label)
+            boxes[h].pop(label, None)
+        else:
+            label, n = word.split("=")
+            h = get_hash(label)
+            boxes[h][label] = n
+
+    focus_powers = []
+    for i, box in enumerate(boxes, start=1):
+        for position, label in enumerate(box.keys(), start=1):
+            focus_powers.append(i * position * int(box[label]))
+
+    return sum(focus_powers)
