bf8914e1eed447155fac7c52f6188cf271ea5c58
Lam <lamnguyenhoang@gmail.com>
2023-12-17 09:32:30 +0100

    Day 17 part 2

--- year2023/days/17/__init__.py
+++ year2023/days/17/__init__.py
@@ -3,8 +3,10 @@ from utils.grid import Grid
 
 
 class City(Grid):
-    def __init__(self, lines):
+    def __init__(self, lines, min_steps, max_steps):
         super().__init__(lines)
+        self.min_steps = min_steps
+        self.max_steps = max_steps
         self.edges = self.build_edges()
 
     def build_edges(self):
@@ -12,25 +14,29 @@ class City(Grid):
         for x, y in self.points:
             for direction in [(1, 0), (-1, 0)]:
                 cost = 0
-                for i in range(1, 4):
+                for i in range(1, self.max_steps + 1):
                     point = (x, y)
                     destination = (x + i * direction[0], y + i * direction[1])
                     if self.grid.get(destination) is None:
                         continue
                     cost += int(self.grid[destination])
+                    if i < self.min_steps:
+                        continue
                     edges.append(((point, "v"), (destination, "h"), cost))
             for direction in [(0, 1), (0, -1)]:
                 cost = 0
-                for i in range(1, 4):
+                for i in range(1, self.max_steps + 1):
                     point = (x, y)
                     destination = (x + i * direction[0], y + i * direction[1])
                     if self.grid.get(destination) is None:
                         continue
                     cost += int(self.grid[destination])
+                    if i < self.min_steps:
+                        continue
                     edges.append(((point, "h"), (destination, "v"), cost))
         return edges
 
-    def do_part_1(self):
+    def get_shortest_path(self):
         return min(
             [
                 djikstra(self.edges, ((0, 0), "h"), ((self.maxx, self.maxy), "h"))[0],
@@ -46,13 +52,13 @@ def process_input(blob):
 
 
 def do_part_1(lines):
-    city = City(lines)
-    z = city.do_part_1()
-    return z
+    city = City(lines, 1, 3)
+    return city.get_shortest_path()
 
 
 def do_part_2(lines):
-    return "toto"
+    city = City(lines, 4, 10)
+    return city.get_shortest_path()
 
 
 def do_visualization(processed_input):
